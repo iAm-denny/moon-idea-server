009aa5f85c612d4fbf5f36783690691abe969780
MLP <minlapyae8@gmail.com>
2022-09-29 13:24:26 +0630

    Added New API - fetch project detail shapes

--- app/controllers/client.controller.js
+++ app/controllers/client.controller.js
@@ -1,9 +1,5 @@
-const mongoose = require('mongoose');
-const { v4 } = require('uuid');
 const Project = require('../models/project.model');
 const Shape = require('../models/shape.model');
-
-const { Schema } = mongoose;
 
 exports.createProject = (req, res) => {
   const { project_name, project_type } = req.body;
@@ -37,18 +33,25 @@ exports.fetchProjects = async (req, res) => {
 exports.shape = async (req, res) => {
   const { project_id, data, id } = req.body;
   try {
-    const shape = new Shape({
+    return Shape.findOneAndUpdate({ id, project_id }, {
       id,
       project_id,
       data,
       created_by: req.user.id,
-    });
-    return shape.save(async (err, response) => {
-      if (err) {
-        return res.status(302).send({ message: err });
-      }
-      return res.status(200).send({ message: 'success', response });
-    });
+    }, { new: true, upsert: true })
+      .then(() => res.status(200).send({ message: 'success', success: true }))
+
+      .catch((err) => console.log('err', err));
+  } catch (err) {
+    return res.json({ message: 'something went wrong', success: false });
+  }
+};
+
+exports.fetchProjectShape = async (req, res) => {
+  const { project_id } = req.query;
+  try {
+    const data = await Shape.find({ project_id, created_by: req.user.id });
+    return res.json({ message: 'success', success: true, data });
   } catch (err) {
     return res.json({ message: 'something went wrong', success: false });
   }
--- app/routes/client.js
+++ app/routes/client.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const verifyToken = require('../../middlewares/authorization');
-const { createProject, fetchProjects, shape } = require('../controllers/client.controller');
+const {
+  createProject, fetchProjects, shape, fetchProjectShape,
+} = require('../controllers/client.controller');
 
 const router = Router();
 
 router.post('/create-project', verifyToken, createProject);
 router.get('/fetch-project', verifyToken, fetchProjects);
 router.post('/project-shape', verifyToken, shape);
+router.get('/project-shape', verifyToken, fetchProjectShape);
 
 module.exports = router;
